81ab305ceec2d81f87f7bdaf0571a49fd965877a
crkntk <crkntk@gmail.com>
2024-08-01 01:03:41 -0700

    Added icon

--- index.js
+++ index.js
@@ -5,19 +5,11 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
-
-
 const key = process.env.PMAP_KEY ;
-
-
-
 const app = express();
-
 const port = 3000;
 const ipifyUrl = "https://api.ipify.org?format=json";
 const ipapiUrl = "https://ipapi.co/"
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
@@ -25,6 +17,7 @@ app.use(express.static("public"));
 
 
 app.get('/', async (req, res) => {
+    // Fetch the user's IP address using the IPify API
     const ippAdd=  await axios.get(ipifyUrl);
     let location;
     try {
@@ -35,7 +28,7 @@ app.get('/', async (req, res) => {
         console.error("Error fetching location data:", err.message);
         return res.status(500).send("Error fetching location data.");
     }
-
+    //render webpage with the papimap key and the location data 
     res.render('homepage.ejs',{
         locationObj: location.data,
         papKey: key
--- public/maps/homepage_map.js
+++ public/maps/homepage_map.js
@@ -3,14 +3,13 @@
 import * as L from "https://cdn.jsdelivr.net/npm/leaflet@1.8.0/dist/leaflet-src.esm.js"
 const key = $("#initmap-script").attr('papKey').toString();
 //const key = '5bhyUK4PAmD0Awuvuu5Q';
-console.log(key);
-console.log("LOADED MAP SCRIPT");
-console.log(Number($("#longitude")[0].innerHTML));
+//aquire longitude and latitude data
 const currUserLat = Number($("#latitude")[0].innerHTML);
 const curUserLong = Number($("#longitude")[0].innerHTML);
+// create map object in order display map
 const map = L.map('map', {
     center: L.latLng(currUserLat, curUserLong),
-    zoom: 20
+    zoom: 14
 });
 L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`, {
     maxZoom: 19,
@@ -20,13 +19,8 @@ L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}
 
 
 var myIcon = L.icon({
-    iconUrl: 'my-icon.png',
-    iconSize: [38, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-    shadowUrl: 'my-icon-shadow.png',
-    shadowSize: [68, 95],
-    shadowAnchor: [22, 94]
+    iconUrl: '/images/ninja-svgrepo-com.svg',
+    iconSize: [20, 20],
 });
 
 
