a08843df99f8d0261420b1d6def01b51f788e43f
crkntk <crkntk@gmail.com>
2024-08-01 03:58:04 -0700

    structured call

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
 const key = process.env.PMAP_KEY ;
+const LokIQ =  process.env.LOCATIONIQ_TOKEN ;
 const app = express();
 const port = 3000;
 const ipifyUrl = "https://api.ipify.org?format=json";
@@ -13,6 +14,7 @@ const ipapiUrl = "https://ipapi.co/"
 app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
+const locIQAPI = "https://us1.locationiq.com/v1/search?key=";
 
 
 
@@ -28,6 +30,7 @@ app.get('/', async (req, res) => {
         console.error("Error fetching location data:", err.message);
         return res.status(500).send("Error fetching location data.");
     }
+    const LocIq_Loc = await axios.get(locIQAPI+LokIQ+ "%q="
     //render webpage with the papimap key and the location data 
     res.render('homepage.ejs',{
         locationObj: location.data,
@@ -38,3 +41,20 @@ app.get('/', async (req, res) => {
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
 });
+let User1 = {
+    address: '23 E. Correll Rd',
+    city: 'Heber',
+    state: 'CA',
+    zip: '92249',
+    country: 'USA',
+    name: 'John Doe 1',
+}
+let User2 = {
+    address: '23 E. Correll Rd',
+    city: 'Heber',
+    state: 'CA',
+    zip: '92249',
+    country: 'USA',
+    name: 'John Doe 1',
+}
+const TempUsers = [User1, User2]
