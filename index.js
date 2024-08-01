713dd3d99333a212a8b753e714159d9556938bae
crkntk <crkntk@gmail.com>
2024-08-01 02:55:20 -0700

    sign-in and sign up html creation

--- index.js
+++ index.js
@@ -14,25 +14,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
 
-
+app.get('/home', async(req, res) =>{
+ // Fetch the user's IP address using the IPify API
+ const ippAdd=  await axios.get(ipifyUrl);
+ let location;
+ try {
+     //console.log(ippAdd);
+  location = await axios.get(ipapiUrl + ippAdd.data.ip + "/json/");
+ }
+ catch (err) {
+     console.error("Error fetching location data:", err.message);
+     return res.status(500).send("Error fetching location data.");
+ }
+ //render webpage with the papimap key and the location data 
+ res.render('homepage.ejs',{
+     locationObj: location.data,
+     papKey: key
+     });
+});
 
 app.get('/', async (req, res) => {
-    // Fetch the user's IP address using the IPify API
-    const ippAdd=  await axios.get(ipifyUrl);
-    let location;
-    try {
-        //console.log(ippAdd);
-     location = await axios.get(ipapiUrl + ippAdd.data.ip + "/json/");
-    }
-    catch (err) {
-        console.error("Error fetching location data:", err.message);
-        return res.status(500).send("Error fetching location data.");
-    }
-    //render webpage with the papimap key and the location data 
-    res.render('homepage.ejs',{
-        locationObj: location.data,
-        papKey: key
-        });
+   res.sendFile(__dirname + '/public/sign_in.html');
 });
 
 app.listen(port, function() {
