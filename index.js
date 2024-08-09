d19cc00d06296f547570e1917ae03f8016ce71f3
crkntk <crkntk@gmail.com>
2024-08-08 20:04:00 -0700

    arrow on goo position

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ app.get('/home', async (req, res) => {
     // Fetch the user's IP address using the IPify API
     const ippAdd=  await axios.get(ipifyUrl);
     let location;
-    try {
-     location = await axios.get(ipapiUrl + ippAdd.data.ip + "/json/");
-    }
-    catch (err) {
-        console.error("Error fetching location data:", err.message);
-        return res.status(500).send("Error fetching location data.");
-    };
+       // try {
+       // location = await axios.get(ipapiUrl + ippAdd.data.ip + "/json/");
+       // }
+       // catch (err) {
+       //     console.error("Error fetching location data:", err.message);
+       //     return res.status(500).send("Error fetching location data.");
+       // };
     let LocIq_Loc;
     // construct the LokIQ API query URL with the user's address, city, state, and zip code
     const Addquery = locIQAPI+LokIQ+ "&q=" +currUserTest.address + "%2C%20" + currUserTest.city + "%2C%20" + currUserTest.state + "%2C%20" + currUserTest.zip + "%20&format=json";
@@ -59,7 +59,6 @@ app.get('/home', async (req, res) => {
     var resultLocIQ = LocIq_Loc.data[0];
     //render webpage with the papimap key and the location data 
     res.render('homepage.ejs',{
-        locationObj: location.data,
         papKey: key,
         lat: resultLocIQ.lat,
         lon: resultLocIQ.lon,
