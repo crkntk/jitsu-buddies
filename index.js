ae00125ae8819c4469382e22c5ade9132820182d
crkntk <crkntk@gmail.com>
2025-08-21 03:33:57 -0700

    adding spatial db

--- index.js
+++ index.js
@@ -47,13 +47,7 @@ app.get('/home', async (req, res) => {
     // Fetch the user's IP address using the IPify API
     const ippAdd=  await axios.get(ipifyUrl);
     let location;
-       // try {
-       // location = await axios.get(ipapiUrl + ippAdd.data.ip + "/json/");
-       // }
-       // catch (err) {
-       //     console.error("Error fetching location data:", err.message);
-       //     return res.status(500).send("Error fetching location data.");
-       // };
+    
     let LocIq_Loc;
     // construct the LokIQ API query URL with the user's address, city, state, and zip code
     const Addquery = locIQAPI+LokIQ+ "&q=" +currUserTest.address + "%2C%20" + currUserTest.city + "%2C%20" + currUserTest.state + "%2C%20" + currUserTest.zip + "%20&format=json";
@@ -84,7 +78,18 @@ app.get('/', async (req, res) => {
 app.post('/createUser', async (req, res) => {
    console.log(req.body);
    
-
+     let LocIq_Loc;
+    // construct the LokIQ API query URL with the user's address, city, state, and zip code
+    const Addquery = locIQAPI+LokIQ+ "&q=" +currUserTest.address + "%2C%20" + currUserTest.city + "%2C%20" + currUserTest.state + "%2C%20" + currUserTest.zip + "%20&format=json";
+    // Calling api to fetch location of latitude and longitude based on address we query locatoniq
+    try{
+    LocIq_Loc = await axios.get(Addquery);
+    }
+    catch(err){
+        console.error("Error fetching location data from LokIQ:", err.message);
+        return res.status(500).send("Error fetching location data from LokIQ.");
+    }
+    var resultLocIQ = LocIq_Loc.data[0];
 
 
 });
